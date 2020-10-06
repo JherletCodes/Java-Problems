90ea29453f0817a5432d0bf85fa9b362dfd35453
jherlet21 <53713996+jherlet21@users.noreply.github.com>
2020-10-07 03:48:12 +0800

    Create Print-Ten-Numbers.java

---
+++ Looping/Print-Ten-Numbers.java
@@ -0,0 +1,21 @@
+package Java;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+  
+		//the loop is for loop
+    
+		for (int i=0;i<=10;i++) {
+			
+      // the loop continues to process unitl it meets its condition
+      
+			System.out.println(i);
+      
+      // prints the variable i as it increments by 1
+      
+		}
+
+	}
+
+}
