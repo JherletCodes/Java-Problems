2611071643a121d1989ce8ea15d44a50aa1525dc
jherlet21 <53713996+jherlet21@users.noreply.github.com>
2020-10-07 03:36:30 +0800

    Create Sum.java

---
+++ Variables/Sum.java
@@ -0,0 +1,17 @@
+package Java;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		int sum=0;
+		int a=5;
+		int b=6;
+		
+		sum=a+b;
+		
+		System.out.println("The sum is: "+sum);
+
+	}
+
+}
